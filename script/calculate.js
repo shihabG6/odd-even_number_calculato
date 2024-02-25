0aec1abf7c1a40712d1f1db496063e11e4facf8e
shihabG6 <shihabuddin0194@gmail.com>
2024-02-25 17:53:35 +0600

    added a simput calculator structure

---
+++ script/calculate.js
@@ -0,0 +1,13 @@
+
+
+let num =0;
+function input(){
+  const inputText = document.getElementById('input1');
+   const number = parseInt(inputText.value);
+   num = num + number
+  
+  
+}
+console.log(num)
+
+
